a97700931d9b2a160c052a7cfc2606688ca21e16
lixiuxian <15573097235@163.com>
2024-01-25 16:57:46 +0800

    feat: 安装UI组件库 navie

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import '@/plugins/tailwind.css'
+import 'vfonts/Lato.css'
+import 'vfonts/FiraCode.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
